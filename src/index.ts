b3667e772680d4bf972d473ce5062479bde88867
Philipp Christen <philpp.christen@srf.ch>
2022-02-04 16:20:01 +0100

    add to export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import TestComponent from "./TestComponent";
-import { UpdateIcon, ExclamationMarkIcon } from './Icon/Icon';
+import { UpdateIcon, ExclamationMarkIcon, SettingsIcon } from './Icon/Icon';
 
-export { TestComponent, UpdateIcon, ExclamationMarkIcon };
+export { TestComponent, UpdateIcon, ExclamationMarkIcon, SettingsIcon };
