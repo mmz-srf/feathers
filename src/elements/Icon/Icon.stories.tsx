68383757bc0f0d87e7d2a5b4bb64e3b8a7850dd2
Patrick Bonetti <patrick.bonetti@srf.ch>
2022-04-04 20:30:16 +0200

    last batch of icons
    SRFCMSAL-5641

--- src/elements/Icon/Icon.stories.tsx
+++ src/elements/Icon/Icon.stories.tsx
@@ -33,6 +33,76 @@ import {
   EdithIcon,
   CaretIcon,
   SpinnerIcon,
+  CheckmarkIcon,
+  TrashcanIcon,
+  DangerIcon,
+  WarningIcon,
+  SuccessIcon,
+  EditArticleIcon,
+  EditTickerItemIcon,
+  RevertIcon,
+  ForbiddenIcon,
+  PlusCircleIcon,
+  PreviewIcon,
+  PodcastIcon,
+  FocusIcon,
+  RotateBackIcon,
+  RotateForwardIcon,
+  ZoomInIcon,
+  ZoomOutIcon,
+  DropAddIcon,
+  DropDownIcon,
+  DropUpIcon,
+  DropDeleteIcon,
+  AudioIcon,
+  VideoIcon,
+  MeteoMapIcon,
+  VideoeditorIcon,
+  AudioeditorIcon,
+  ArticleIcon,
+  AudioFeedIcon,
+  FeedIcon,
+  VideoFeedIcon,
+  TeaserIcon,
+  EmptyIcon,
+  OpinionIcon,
+  LongformIcon,
+  LiveIcon,
+  TVIcon,
+  FacebookIcon,
+  TimeIcon,
+  InvisibleIcon,
+  HeadingIcon,
+  BoldIcon,
+  ItalicIcon,
+  UnorderedListIcon,
+  OrderedListIcon,
+  LinkIcon,
+  UnlinkIcon,
+  CommentIcon,
+  PolisMapIcon,
+  FederalVotesIcon,
+  CHMapIcon,
+  CropIcon,
+  PushIcon,
+  MaterialBoxIcon,
+  QuoteIcon,
+  ExternalLinkIcon,
+  ImportImageIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  RawHTMLIcon,
+  FormIcon,
+  NewsGraphicIcon,
+  PollIcon,
+  DeleteColumnIcon,
+  AddColumnIcon,
+  DeleteRowIcon,
+  AddRowIcon,
+  TableIcon,
+  TableHeadIcon,
+  TableWidthIcon,
+  InstagramIcon,
 } from "./Icon";
 
 export default {
@@ -61,6 +131,7 @@ export const Linkbox = () => <LinkboxIcon />;
 export const Plus = () => <PlusIcon />;
 export const Publish = () => <PublishIcon />;
 export const PublishAndLeave = () => <PublishAndLeaveIcon />;
+export const SaveandLeave = () => <SaveAndLeaveIcon />;
 export const Save = () => <SaveIcon />;
 export const Retire = () => <RetireIcon />;
 export const SchorschLogo = () => <SchorschLogoIcon />;
@@ -78,5 +149,73 @@ export const Edith = () => <EdithIcon />;
 export const Caret = () => <CaretIcon />;
 export const Spinner = () => <SpinnerIcon />;
 export const SpinnerWithBrandColor = () => <SpinnerIcon modifier="branding" />;
-
-//export const Settings = () => <SettingsIcon />;
+export const Checkmark = () => <CheckmarkIcon />;
+export const Trashcan = () => <TrashcanIcon />;
+export const Danger = () => <DangerIcon />;
+export const Warning = () => <WarningIcon />;
+export const Success = () => <SuccessIcon />;
+export const EditArticle = () => <EditArticleIcon />;
+export const EditTickerItem = () => <EditTickerItemIcon />;
+export const Revert = () => <RevertIcon />;
+export const Forbidden = () => <ForbiddenIcon />;
+export const PlusCircle = () => <PlusCircleIcon />;
+export const Preview = () => <PreviewIcon />;
+export const Podcast = () => <PodcastIcon />;
+export const Focus = () => <FocusIcon />;
+export const RotateBack = () => <RotateBackIcon />;
+export const RotateForward = () => <RotateForwardIcon />;
+export const ZoomIn = () => <ZoomInIcon />;
+export const ZoomOut = () => <ZoomOutIcon />;
+export const DropAdd = () => <DropAddIcon />;
+export const DropDown = () => <DropDownIcon />;
+export const DropUp = () => <DropUpIcon />;
+export const DropDelete = () => <DropDeleteIcon />;
+export const Audio = () => <AudioIcon />;
+export const Video = () => <VideoIcon />;
+export const MeteoMap = () => <MeteoMapIcon />;
+export const Videoeditor = () => <VideoeditorIcon />;
+export const Audioeditor = () => <AudioeditorIcon />;
+export const Article = () => <ArticleIcon />;
+export const AudioFeed = () => <AudioFeedIcon />;
+export const Feed = () => <FeedIcon />;
+export const Teaser = () => <TeaserIcon />;
+export const VideoFeed = () => <VideoFeedIcon />;
+export const Empty = () => <EmptyIcon />;
+export const Opinion = () => <OpinionIcon />;
+export const Longform = () => <LongformIcon />;
+export const Live = () => <LiveIcon />;
+export const TV = () => <TVIcon />;
+export const Facebook = () => <FacebookIcon />;
+export const Time = () => <TimeIcon />;
+export const Invisible = () => <InvisibleIcon />;
+export const Heading = () => <HeadingIcon />;
+export const Bold = () => <BoldIcon />;
+export const Italic = () => <ItalicIcon />;
+export const UnorderedList = () => <UnorderedListIcon />;
+export const OrderedList = () => <OrderedListIcon />;
+export const Link = () => <LinkIcon />;
+export const Unlink = () => <UnlinkIcon />;
+export const Comment = () => <CommentIcon />;
+export const PolisMap = () => <PolisMapIcon />;
+export const FederalVotes = () => <FederalVotesIcon />;
+export const CHMap = () => <CHMapIcon />;
+export const Crop = () => <CropIcon />;
+export const Push = () => <PushIcon />;
+export const MaterialBox = () => <MaterialBoxIcon />;
+export const Quote = () => <QuoteIcon />;
+export const ExternalLink = () => <ExternalLinkIcon />;
+export const ImportImage = () => <ImportImageIcon />;
+export const ArrowDown = () => <ArrowDownIcon />;
+export const ArrowUp = () => <ArrowUpIcon />;
+export const RawHTML = () => <RawHTMLIcon />;
+export const Form = () => <FormIcon />;
+export const NewsGraphic = () => <NewsGraphicIcon />;
+export const Poll = () => <PollIcon />;
+export const Table = () => <TableIcon />;
+export const AddRow = () => <AddRowIcon />;
+export const DeleteRow = () => <DeleteRowIcon />;
+export const AddColumn = () => <AddColumnIcon />;
+export const DeleteColumn = () => <DeleteColumnIcon />;
+export const TableHead = () => <TableHeadIcon />;
+export const TableWidth = () => <TableWidthIcon />;
+export const Instagram = () => <InstagramIcon />;
--- src/elements/Icon/Icon.tsx
+++ src/elements/Icon/Icon.tsx
@@ -36,16 +36,85 @@ import Twitter from "./Images/Twitter.svg";
 import Edith from "./Images/Edith.svg";
 import Caret from "./Images/Caret.svg";
 import Spinner from "./Images/Spinner.svg";
+import Checkmark from "./Images/Checkmark.svg";
+import Trashcan from "./Images/Trashcan.svg";
+import Danger from "./Images/Danger.svg";
+import Warning from "./Images/Warning.svg";
+import Success from "./Images/Success.svg";
+import EditArticle from "./Images/EditArticle.svg";
+import EditTickerItem from "./Images/EditTickerItem.svg";
+import Revert from "./Images/Revert.svg";
+import Forbidden from "./Images/Forbidden.svg";
+import PlusCircle from "./Images/PlusCircle.svg";
+import Preview from "./Images/Preview.svg";
+import Podcast from "./Images/Podcast.svg";
+import Focus from "./Images/Focus.svg";
+import RotateBack from "./Images/RotateBack.svg";
+import RotateForward from "./Images/RotateForward.svg";
+import ZoomIn from "./Images/ZoomIn.svg";
+import ZoomOut from "./Images/ZoomOut.svg";
+import DropAdd from "./Images/DropAdd.svg";
+import DropDown from "./Images/DropDown.svg";
+import DropUp from "./Images/DropUp.svg";
+import DropDelete from "./Images/DropDelete.svg";
+import Audio from "./Images/Audio.svg";
+import Video from "./Images/Video.svg";
+import MeteoMap from "./Images/MeteoMap.svg";
+import Videoeditor from "./Images/Videoeditor.svg";
+import Audioeditor from "./Images/Audioeditor.svg";
+import Article from "./Images/Article.svg";
+import AudioFeed from "./Images/AudioFeed.svg";
+import Feed from "./Images/Feed.svg";
+import Teaser from "./Images/Teaser.svg";
+import VideoFeed from "./Images/VideoFeed.svg";
+import Empty from "./Images/Empty.svg";
+import Opinion from "./Images/Opinion.svg";
+import Longform from "./Images/Longform.svg";
+import Live from "./Images/Live.svg";
+import TV from "./Images/TV.svg";
+import Facebook from "./Images/Facebook.svg";
+import Time from "./Images/Time.svg";
+import Invisible from "./Images/Invisible.svg";
+import Heading from "./Images/Heading.svg";
+import Bold from "./Images/Bold.svg";
+import Italic from "./Images/Italic.svg";
+import UnorderedList from "./Images/UnorderedList.svg";
+import OrderedList from "./Images/OrderedList.svg";
+import Link from "./Images/Link.svg";
+import Unlink from "./Images/Unlink.svg";
+import Comment from "./Images/Comment.svg";
+import PolisMap from "./Images/PolisMap.svg";
+import FederalVotes from "./Images/FederalVotes.svg";
+import CHMap from "./Images/CHMap.svg";
+import Crop from "./Images/Crop.svg";
+import Push from "./Images/Push.svg";
+import MaterialBox from "./Images/MaterialBox.svg";
+import Quote from "./Images/Quote.svg";
+import ExternalLink from "./Images/ExternalLink.svg";
+import ImportImage from "./Images/ImportImage.svg";
+import RawHTML from "./Images/RawHTML.svg";
+import Form from "./Images/Form.svg";
+import NewsGraphic from "./Images/NewsGraphic.svg";
+import Poll from "./Images/Poll.svg";
+import Table from "./Images/Table.svg";
+import AddRow from "./Images/AddRow.svg";
+import DeleteRow from "./Images/DeleteRow.svg";
+import AddColumn from "./Images/AddColumn.svg";
+import DeleteColumn from "./Images/DeleteColumn.svg";
+import TableHead from "./Images/TableHead.svg";
+import TableWidth from "./Images/TableWidth.svg";
+import Instagram from "./Images/Instagram.svg";
 
 import "./Icon.scss";
 
-const Icon: React.FC<IconProps> = ({ modifier, children }) => (
+const Icon: React.FC<IconProps> = ({ modifier, noStroke, children }) => (
   <div
     data-testid="Icon"
     className={classNames("f-icon", {
       "f-icon--success": modifier === "success",
       "f-icon--danger": modifier === "danger",
       "f-icon--info": modifier === "info",
+      "f-icon--no-stroke": noStroke,
     })}
   >
     {children}
@@ -226,9 +295,12 @@ export const PeopleIcon: React.FC<IconProps> = ({ modifier }) => (
   </Icon>
 );
 
-export const TwitterIcon: React.FC<IconProps> = ({ modifier }) => (
+export const TwitterIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Twitter",
+}) => (
   <Icon {...{ modifier }}>
-    <Twitter />
+    <Twitter title={alt} />
   </Icon>
 );
 
@@ -251,6 +323,495 @@ export const SpinnerIcon: React.FC<IconProps> = ({ modifier }) => (
     ) : (
       <Spinner />
     )}
+  </Icon>
+);
+
+export const CheckmarkIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Checkmark />
+  </Icon>
+);
+
+export const TrashcanIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Trashcan />
+  </Icon>
+);
+
+export const DangerIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon noStroke={true} {...{ modifier }}>
+    <Danger />
+  </Icon>
+);
+
+export const WarningIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon noStroke={true} {...{ modifier }}>
+    <Warning />
+  </Icon>
+);
+
+export const SuccessIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon noStroke={true} {...{ modifier }}>
+    <Success />
+  </Icon>
+);
+
+export const EditArticleIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <EditArticle />
+  </Icon>
+);
+
+export const EditTickerItemIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <EditTickerItem />
+  </Icon>
+);
+
+export const RevertIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Revert />
+  </Icon>
+);
+
+export const ForbiddenIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Forbidden />
+  </Icon>
+);
+
+export const PlusCircleIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <PlusCircle />
+  </Icon>
+);
+
+export const PreviewIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Preview />
+  </Icon>
+);
+
+export const PodcastIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Podcast />
+  </Icon>
+);
+
+export const FocusIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Focus />
+  </Icon>
+);
+
+export const RotateBackIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <RotateBack />
+  </Icon>
+);
+
+export const RotateForwardIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <RotateForward />
+  </Icon>
+);
+
+export const ZoomInIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <ZoomIn />
+  </Icon>
+);
+
+export const ZoomOutIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <ZoomOut />
+  </Icon>
+);
+
+export const DropAddIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <DropAdd />
+  </Icon>
+);
+
+export const DropDownIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <DropDown />
+  </Icon>
+);
+
+export const DropUpIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <DropUp />
+  </Icon>
+);
+
+export const DropDeleteIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <DropDelete />
+  </Icon>
+);
+
+export const AudioIcon: React.FC<IconProps> = ({ modifier, alt = "Audio" }) => (
+  <Icon noStroke={true} {...{ modifier }}>
+    <Audio title={alt} />
+  </Icon>
+);
+
+export const VideoIcon: React.FC<IconProps> = ({ modifier, alt = "Video" }) => (
+  <Icon noStroke={true} {...{ modifier }}>
+    <Video title={alt} />
+  </Icon>
+);
+
+export const MeteoMapIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <MeteoMap />
+  </Icon>
+);
+
+export const VideoeditorIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Videoeditor />
+  </Icon>
+);
+
+export const AudioeditorIcon: React.FC<IconProps> = ({ modifier }) => (
+  <Icon {...{ modifier }}>
+    <Audioeditor />
+  </Icon>
+);
+
+export const ArticleIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Article",
+}) => (
+  <Icon {...{ modifier }}>
+    <Article title={alt} />
+  </Icon>
+);
+
+export const AudioFeedIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Audio-Feed",
+}) => (
+  <Icon {...{ modifier }}>
+    <AudioFeed title={alt} />
+  </Icon>
+);
+
+export const FeedIcon: React.FC<IconProps> = ({ modifier, alt = "Feed" }) => (
+  <Icon {...{ modifier }}>
+    <Feed title={alt} />
+  </Icon>
+);
+
+export const TeaserIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Teaser",
+}) => (
+  <Icon {...{ modifier }}>
+    <Teaser title={alt} />
+  </Icon>
+);
+
+export const VideoFeedIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Video-Feed",
+}) => (
+  <Icon {...{ modifier }}>
+    <VideoFeed title={alt} />
+  </Icon>
+);
+
+export const EmptyIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Empty title={alt} />
+  </Icon>
+);
+
+export const OpinionIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Opinion title={alt} />
+  </Icon>
+);
+
+export const LongformIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Longform title={alt} />
+  </Icon>
+);
+
+export const LiveIcon: React.FC<IconProps> = ({ modifier, alt = "Live" }) => (
+  <Icon {...{ modifier }}>
+    <Live title={alt} />
+  </Icon>
+);
+
+export const TVIcon: React.FC<IconProps> = ({ modifier, alt = "TV" }) => (
+  <Icon {...{ modifier }}>
+    <TV title={alt} />
+  </Icon>
+);
+
+export const FacebookIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Facebook",
+}) => (
+  <Icon {...{ modifier }}>
+    <Facebook title={alt} />
+  </Icon>
+);
+
+export const TimeIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Time title={alt} />
+  </Icon>
+);
+
+export const InvisibleIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Invisible title={alt} />
+  </Icon>
+);
+
+export const HeadingIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Überschrift",
+}) => (
+  <Icon {...{ modifier }}>
+    <Heading title={alt} />
+  </Icon>
+);
+
+export const BoldIcon: React.FC<IconProps> = ({ modifier, alt = "Fett" }) => (
+  <Icon {...{ modifier }}>
+    <Bold title={alt} />
+  </Icon>
+);
+
+export const ItalicIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Kursiv",
+}) => (
+  <Icon {...{ modifier }}>
+    <Italic title={alt} />
+  </Icon>
+);
+
+export const UnorderedListIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Liste mit Punkten",
+}) => (
+  <Icon {...{ modifier }}>
+    <UnorderedList title={alt} />
+  </Icon>
+);
+
+export const OrderedListIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Nummerierte Liste",
+}) => (
+  <Icon {...{ modifier }}>
+    <OrderedList title={alt} />
+  </Icon>
+);
+
+export const LinkIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Verlinkung erstellen",
+}) => (
+  <Icon {...{ modifier }}>
+    <Link title={alt} />
+  </Icon>
+);
+
+export const UnlinkIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Verlinkung aufheben",
+}) => (
+  <Icon {...{ modifier }}>
+    <Unlink title={alt} />
+  </Icon>
+);
+
+export const CommentIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Kommentare",
+}) => (
+  <Icon {...{ modifier }}>
+    <Comment title={alt} />
+  </Icon>
+);
+
+export const PolisMapIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <PolisMap title={alt} />
+  </Icon>
+);
+
+export const FederalVotesIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <FederalVotes title={alt} />
+  </Icon>
+);
+
+export const CHMapIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <CHMap title={alt} />
+  </Icon>
+);
+
+export const CropIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Crop title={alt} />
+  </Icon>
+);
+
+export const PushIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <Push title={alt} />
+  </Icon>
+);
+
+export const MaterialBoxIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Materialbox",
+}) => (
+  <Icon {...{ modifier }}>
+    <MaterialBox title={alt} />
+  </Icon>
+);
+
+export const QuoteIcon: React.FC<IconProps> = ({ modifier, alt = "Zitat" }) => (
+  <Icon {...{ modifier }}>
+    <Quote title={alt} />
+  </Icon>
+);
+
+export const ExternalLinkIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <ExternalLink title={alt} />
+  </Icon>
+);
+
+export const ImportImageIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Bild importieren",
+}) => (
+  <Icon {...{ modifier }}>
+    <ImportImage title={alt} />
+  </Icon>
+);
+
+export const ArrowDownIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <></>
+    {/* TODO
+        <GoArrowDown />
+      */}
+  </Icon>
+);
+
+export const ArrowUpIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <></>
+    {/* TODO
+        <GoArrowUp />
+      */}
+  </Icon>
+);
+
+export const RawHTMLIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <RawHTML title={alt} />
+  </Icon>
+);
+
+export const FormIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Formular",
+}) => (
+  <Icon {...{ modifier }}>
+    <Form title={alt} />
+  </Icon>
+);
+
+export const NewsGraphicIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <NewsGraphic title={alt} />
+  </Icon>
+);
+
+export const PollIcon: React.FC<IconProps> = ({ modifier, alt = "Poll" }) => (
+  <Icon {...{ modifier }}>
+    <Poll title={alt} />
+  </Icon>
+);
+
+export const TableIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Tabelle",
+}) => (
+  <Icon {...{ modifier }}>
+    <Table title={alt} />
+  </Icon>
+);
+
+export const AddRowIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Zeile hinzufügen",
+}) => (
+  <Icon {...{ modifier }}>
+    <AddRow title={alt} />
+  </Icon>
+);
+
+export const DeleteRowIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Zeile entfernen",
+}) => (
+  <Icon {...{ modifier }}>
+    <DeleteRow title={alt} />
+  </Icon>
+);
+
+export const AddColumnIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Spalte hinzufügen",
+}) => (
+  <Icon {...{ modifier }}>
+    <AddColumn title={alt} />
+  </Icon>
+);
+
+export const DeleteColumnIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Spalte entfernen",
+}) => (
+  <Icon {...{ modifier }}>
+    <DeleteColumn title={alt} />
+  </Icon>
+);
+
+export const TableHeadIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <TableHead title={alt} />
+  </Icon>
+);
+
+export const TableWidthIcon: React.FC<IconProps> = ({ modifier, alt }) => (
+  <Icon {...{ modifier }}>
+    <TableWidth title={alt} />
+  </Icon>
+);
+
+export const InstagramIcon: React.FC<IconProps> = ({
+  modifier,
+  alt = "Instagram",
+}) => (
+  <Icon {...{ modifier }}>
+    <Instagram title={alt} />
   </Icon>
 );
 
