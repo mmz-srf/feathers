8a23041e8336007e27012c9945fa6ae5bdd50948
Philipp Christen <philpp.christen@srf.ch>
2022-04-05 10:57:42 +0200

    add enums to tooltips

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import TOOLTIP_CONTENT from './Tooltip-Content';
 import { TooltipProps } from './Tooltip.types';
 
 import './Tooltip.scss';
+
+export enum ToolTipModifiers {
+  AlwaysOpen = 'always_open',
+  SpaceFilling = 'space_filling',
+  NoMinWidth = 'no_min_width',
+  ShowDelayed = 'show_delayed',
+  Validation = 'validation',
+  MaxParentWidth = 'max_parent_width',
+  NoWordWrap = 'no_word_wrap',
+  NoBorder = 'no_border',
+  NoMargin = 'no_margin',
+}
 
 const Tooltip: React.FC<TooltipProps> = ({
   modifier,
@@ -20,25 +31,22 @@ const Tooltip: React.FC<TooltipProps> = ({
       className={classNames(
         'f-tooltip',
         {
-          'f-tooltip--always-open': modifier === 'always_open',
-          'f-tooltip--space-filling': modifier === 'space_filling',
-          'f-tooltip--no-min-width': modifier === 'no_min_width',
-          'f-tooltip--show-delayed': modifier === 'show_delayed',
-          'f-tooltip--validation': modifier === 'validation',
-          'f-tooltip--max-parent-width': modifier === 'max_parent_width',
-          'f-tooltip--no-word-wrap': modifier === 'no_word_wrap',
-          'f-tooltip--no-border': modifier === 'no_border',
-          'f-tooltip--no-margin': modifier === 'no_margin',
+          'f-tooltip--always-open': modifier.includes(ToolTipModifiers.AlwaysOpen),
+          'f-tooltip--space-filling': modifier.includes(ToolTipModifiers.SpaceFilling),
+          'f-tooltip--no-min-width': modifier.includes(ToolTipModifiers.NoMinWidth),
+          'f-tooltip--show-delayed': modifier.includes(ToolTipModifiers.ShowDelayed),
+          'f-tooltip--validation': modifier.includes(ToolTipModifiers.Validation),
+          'f-tooltip--max-parent-width': modifier.includes(ToolTipModifiers.MaxParentWidth),
+          'f-tooltip--no-word-wrap': modifier.includes(ToolTipModifiers.NoWordWrap) || (modifier.includes(ToolTipModifiers.NoMinWidth) && content.length < 30),
+          'f-tooltip--no-border': modifier.includes(ToolTipModifiers.NoBorder),
+          'f-tooltip--no-margin': modifier.includes(ToolTipModifiers.NoMargin),
           'f-tooltip--up': direction === 'up',
           'f-tooltip--right': direction === 'right',
           'f-tooltip--down': direction === 'down',
           'f-tooltip--left': direction === 'left',
           'f-tooltip--down-edge-left': direction === 'down-edge-left',
           'f-tooltip--down-edge-right': direction === 'down-edge-right',
-        }, {
-        [modifier.NO_WORD_WRAP]:
-          modifier.includes(modifier.NO_MIN_WIDTH) && content.length < 30,
-      })}
+        })}
     >
       {text}
       <span className='f-tooltip__content'>
--- src/Tooltip/Tooltip.types.ts
+++ src/Tooltip/Tooltip.types.ts
@@ -1,5 +1,7 @@
+import {ToolTipModifiers} from "./Tooltip";
+
 export interface TooltipProps {
-  modifier?: 'f-tooltip--always-open' | 'f-tooltip--space-filling' | 'f-tooltip--no-min-width' | 'f-tooltip--show-delayed' | 'f-tooltip--validation' | 'f-tooltip--max-parent-width' | 'f-tooltip--no-word-wrap' | 'f-tooltip--no-border' | 'f-tooltip--no-margin';
+  modifier?: ToolTipModifiers | Array<ToolTipModifiers>;
   text: string;
   // text: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   direction?: 'up' | 'right' | 'down' | 'left' | 'down-edge-left' | 'down-edge-right';
