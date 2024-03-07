219186939e56f712c6b912e7791717640eb2578f
Elia Ritzmann <derelia2004@gmail.com>
2024-03-07 13:43:42 +0100

    Add Dropdown component with styles and types

---
+++ src/elements/Dropdown/Dropdown.tsx
@@ -0,0 +1,159 @@
+import React, { useRef, useState } from 'react';
+import classNames from 'classnames';
+
+//Faerhers components
+import Button, { ButtonModifiersType, ButtonTooltip } from '../Button/Button';
+import { CaretIcon, IconModifiersType } from '../Icon/Icon';
+
+import { DropdownPaneProps, DropdownButtonProps } from './Dropdown.types';
+
+import './Dropdown.scss';
+
+// DropdownPane
+export const DropdownPaneModifiers = [
+  'scrollable',
+  'transparent',
+  'open_upwards',
+  'open_to_right',
+] as const;
+
+export type DropdownPaneModifiersType = (typeof DropdownPaneModifiers)[number];
+
+const DropdownPane: React.FC<DropdownPaneProps> = ({
+  modifier,
+  children,
+  ref,
+}) => (
+  <div
+    className={classNames('f-dropdown-panel', {
+      'f-dropdown-panel--scrollable': modifier?.includes('scrollable'),
+      'f-dropdown-panel--transparent': modifier?.includes('transparent'),
+      'f-dropdown-panel--upside-down': modifier?.includes('open_upwards'),
+      'f-dropdown-panel--left': modifier?.includes('open_to_right'),
+    })}
+    ref={ref}>
+    {children}
+  </div>
+);
+
+//DropdownButton
+const DropdownButton = ({
+  onClick,
+  icon,
+  modifier,
+  childModifier = [],
+  disabled = false,
+  text = '',
+  textOpen = text,
+  tooltip = null,
+  children,
+  inProgress = false,
+  initiallyOpen = false,
+  //badge = false,
+  dataCy = null,
+}: DropdownButtonProps) => {
+  const [open, setOpen] = useState(initiallyOpen);
+  const dropdownPanelRef = useRef(null);
+  const dropdownButtonRef = useRef(null);
+
+  // useOutsideClickListener hook
+  // useOutsideClickListener(dropdownPanelRef, (event) => {
+  //   if (
+  //     dropdownButtonRef.current &&
+  //     !dropdownButtonRef.current.contains(event.target)
+  //   ) {
+  //     setOpen(false);
+  //   }
+  // });
+
+  const handleDropdownPanelClick = (event) => {
+    if (dropdownPanelRef.current?.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
+  const onCaretClick = () => {
+    if (open) {
+      (document.activeElement as HTMLElement).blur();
+    }
+    setOpen(!open);
+  };
+
+  const onMainButtonClick = (event) => {
+    if (onClick) {
+      onClick(event);
+    } else {
+      onCaretClick();
+    }
+  };
+
+  if (open && inProgress) {
+    setOpen(false);
+  }
+
+  return (
+    <div
+      className={classNames('f-dropdown-button', modifier)}
+      ref={dropdownButtonRef}
+      data-cy={dataCy}
+      onClick={handleDropdownPanelClick}>
+      <Button
+        icon={icon}
+        text={open ? textOpen : text}
+        disabled={disabled || inProgress}
+        onClick={onMainButtonClick}
+        modifier={
+          [
+            'no_outline',
+            ...childModifier,
+            inProgress ? 'invisible_content' : '',
+          ] as Array<ButtonModifiersType>
+        }
+        tooltip={!open && tooltip ? tooltip : null}
+      />
+
+      <Button
+        icon={
+          <CaretIcon
+            modifier={
+              [
+                'rotatable',
+                open ? 'rotated-180' : '',
+              ] as Array<IconModifiersType>
+            }
+          />
+        }
+        disabled={disabled || inProgress}
+        onClick={onCaretClick}
+        modifier={
+          [
+            'no_outline',
+            ...childModifier,
+            inProgress ? 'invisible_content' : '',
+          ] as Array<ButtonModifiersType>
+        }
+        tooltip={
+          open ? null : (
+            <ButtonTooltip
+              content="Weitere Optionen anzeigen"
+              direction="down-edge-right"
+              modifier={['no_min_width']}
+            />
+          )
+        }
+      />
+
+      {
+        //{badge && <GlobiBadge>{badge}</GlobiBadge>}
+      }
+
+      {open && <DropdownPane ref={dropdownPanelRef}>{children}</DropdownPane>}
+
+      {
+        //{inProgress && <GlobiProgressDisplay />}
+      }
+    </div>
+  );
+};
+
+export { DropdownPane, DropdownButton };
---
+++ src/elements/Dropdown/Dropdown.types.ts
@@ -0,0 +1,26 @@
+import React, { ReactElement, RefObject } from 'react';
+import { DropdownPaneModifiersType } from './Dropdown';
+import { ButtonModifiersType } from '../Button/Button';
+import { IconProps } from '../Icon/Icon.types';
+import { TooltipProps } from '../Tooltip/Tooltip.types';
+
+export interface DropdownPaneProps {
+  modifier?: DropdownPaneModifiersType | Array<DropdownPaneModifiersType>;
+  children: React.ReactNode;
+  ref?: RefObject<HTMLDivElement>;
+}
+
+export interface DropdownButtonProps {
+  onClick: (event) => void;
+  icon?: ReactElement<IconProps, string>;
+  text: string;
+  textOpen?: string;
+  modifier?: string;
+  childModifier?: Array<ButtonModifiersType>;
+  disabled?: boolean;
+  tooltip?: ReactElement<TooltipProps, string>;
+  children: React.ReactNode;
+  inProgress?: boolean;
+  initiallyOpen?: boolean;
+  dataCy?: unknown;
+}
