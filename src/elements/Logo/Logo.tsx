839bb9302300241de70a8c71708d915eed13f827
Elia Ritzmann <derelia2004@gmail.com>
2024-03-11 14:11:27 +0100

    Update Logo component

--- src/elements/Logo/Logo.tsx
+++ src/elements/Logo/Logo.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
-
 import { LogoProps } from './Logo.types';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import globiImage from './images/Globi.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import schorschImage from './images/Schorsch.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import roebiImage from './images/Roebi.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import karlImage from './images/Karl.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import pittImage from './images/Pitt.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import tweetyImage from './images/Tweety.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import hahnImage from './images/Hahn.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import falkImage from './images/Falk.png';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import woodyImage from './images/Woody.png';
+
 import './Logo.scss';
+
+const images = {
+  globi: globiImage,
+  schorsch: schorschImage,
+  roebi: roebiImage,
+  karl: karlImage,
+  pitt: pittImage,
+  tweety: tweetyImage,
+  hahn: hahnImage,
+  falk: falkImage,
+  woody: woodyImage,
+};
 
 export const LogoBirds = [
   'globi',
@@ -18,9 +57,17 @@ export const LogoBirds = [
 
 export type LogoBirdsType = (typeof LogoBirds)[number];
 
+const birdToSrc = (bird: LogoBirdsType) => {
+  return images[bird] || images.globi;
+};
+
 const Logo: React.FC<LogoProps> = ({ bird = 'globi' }) => (
   <div className="f-logo">
-    <div className={`f-logo__image f-logo__image--${bird}`} />
+    <img
+      src={birdToSrc(bird)}
+      alt=""
+      className={`f-logo__image f-logo__image--${bird}`}
+    />
   </div>
 );
 
